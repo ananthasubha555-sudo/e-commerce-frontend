baa5ed05cdc204463f9352b312ae7422b290681c
ananthasubha555-sudo <ananthasubha555@gmail.com>
2026-01-10 22:08:59 +0530

    Update API base URL to use environment variable

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://e-commerce-backend-5-cacl.onrender.com'
+   baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 API.interceptors.request.use((req) => {
